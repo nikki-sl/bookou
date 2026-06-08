fc2d2f0fa3d9a0d718c70d8dfc8cfec2331832a2
Nicole Sousa <nicolesousaml@alu.ufc.br>
2026-06-07 23:40:19 -0300

    Fix: corrige problemas no projeto

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBphWEX4y8UZqcggR-cor7VDq8wAEch7a0",
-  authDomain: "bookou-2026.firebaseapp.com",
-  projectId: "bookou-2026",
-  storageBucket: "bookou-2026.firebasestorage.app",
-  messagingSenderId: "546117524651",
-  appId: "1:546117524651:web:6528e33fd23b2cc999ebee"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 
